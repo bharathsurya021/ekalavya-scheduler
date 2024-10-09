b2e7fc1cfefa66fc9719cc38f194cfb3b4c1aafd
bharathsurya021 <bharathsurya021@gmail.com>
2024-10-09 10:17:14 +0530

    change icon and use grid

--- src/utilities/screen/ScreenCard.jsx
+++ src/utilities/screen/ScreenCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Card, CardContent, Typography, IconButton, Menu, MenuItem } from '@mui/material';
-import { LinearScaleOutlined } from '@mui/icons-material';
+import { Card, CardContent, Typography, IconButton, Menu, MenuItem, Grid } from '@mui/material';
+import { MoreVertOutlined } from '@mui/icons-material';
 
 const ScreenCard = ({ title, id, onOptionClick }) => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -16,14 +16,22 @@ const ScreenCard = ({ title, id, onOptionClick }) => {
   return (
     <Card sx={{ width: '160px', height: '160px' }}>
       <CardContent>
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
-            {title}
-          </Typography>
-          <IconButton aria-controls="screen-options-menu" aria-haspopup="true" onClick={handleMenuOpen}>
-            <LinearScaleOutlined />
-          </IconButton>
-        </div>
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Grid item xs>
+            <Typography noWrap>
+              {title}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <IconButton
+              aria-controls="screen-options-menu"
+              aria-haspopup="true"
+              onClick={handleMenuOpen}
+            >
+              <MoreVertOutlined />
+            </IconButton>
+          </Grid>
+        </Grid>
         <Menu
           id="screen-options-menu"
           anchorEl={anchorEl}
