d3f941dd6905bb9d322dc753b295592df7c34551
bharathsurya021 <bharathsurya021@gmail.com>
2024-10-09 10:19:23 +0530

    add dashboard layout

---
+++ src/layouts/DashboardLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import PageContainer from '../components/containers/PageContainer';
+import DashboardCard from '../utilities/DashboardCard';
+
+const DashboardLayout = ({ title, subtitle, children }) => {
+  return (
+    <PageContainer title={title} description={subtitle}>
+      <Grid container spacing={3}>
+        <Grid item sm={12}>
+          <DashboardCard title={title} subtitle={subtitle}>
+            {children}
+          </DashboardCard>
+        </Grid>
+      </Grid>
+    </PageContainer>
+  );
+};
+
+export default DashboardLayout;
